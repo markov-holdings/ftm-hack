737af733d19740fe3af1330240a5d4f0a54b0390
matthewtnx <71654298+matthewtnx@users.noreply.github.com>
2023-01-16 12:45:57 +0800

    added the form for bot creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import LoggedInPage from "./pages/LoggedInPage/LoggedInPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import LandingPage from "./pages/LandingPage";
-import CreateBotPage from "./pages/CreateBotPage";
+import CreateBotPage from "./pages/CreateBotPage/CreateBotPage";
 import VideoPage from "./pages/VideoPage";
 
 
