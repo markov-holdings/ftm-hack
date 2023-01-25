db8f920526cf99576e59cc56d1928940629ed777
matthewtnx <71654298+matthewtnx@users.noreply.github.com>
2023-01-26 03:16:42 +0800

    Some ui edits
    Changed icons for top of app, name, logo. Added some cards for bot page

--- frontend/src/pages/ManageBotPage/ManageBotPage.jsx
+++ frontend/src/pages/ManageBotPage/ManageBotPage.jsx
@@ -1,13 +1,52 @@
-import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import NavMenu from "../../components/NavMenu";
+import clothing from "../../images/bot-clothing.jpg";
+import flower from "../../images/bot-flower.jpg";
+
 import "./index.scss";
 
+const tabs = [
+  { href: "/user", name: "Home" },
+  { href: "/user/videos", name: "Video" },
+  { href: "/user/managebot", name: "Manage Bot"},
+  { href: "/user/createBot", name: "Bot Creation" },
+
+];
+
+const botServices = [
+  {
+    img: flower,
+    text: "Bot for selling flowers",
+  },
+  {
+    img: clothing,
+    text: "Bot for selling clothings",
+  },
+];
+
 const ManageBotPage = () => {
+  const location = useLocation();
 
   return (
-    <div>
-        
-    </div>
+    <>
+      <NavMenu tabs={tabs} />
+      {location.pathname === "/user/managebot" ? (
+        <div className="manageBotPage">
+          <div className="manageBotPageTitle">Click cards to know mroe or edit the bots</div>
+          <div className="manageBotPageCards">
+            {botServices.map((service) => (
+              <div className="manageBotPageCard">
+                <img src={service.img} alt={service.text} />
+                <div>{service.text}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Outlet />
+      )}
+    </>
   );
 };
 
